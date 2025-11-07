b8e3ce6f86da8d5f168f90bf0e0fc06dea2fedbd
bingMM <michael.madelaire@uib.no>
2025-11-07 16:36:13 +0100

    2025_11_7 EOD

--- icbuilder/__init__.py
+++ icbuilder/__init__.py
@@ -3,12 +3,14 @@
 from .preimage import PreImage
 from .binnedimage import BinnedImage
 from .conductanceimage import ConductanceImage
+from .splineimage import SplineImage
 from .imagesat_e0_eflux_estimates import E0_eflux_propagated as confun
 
 __all__ = [
     "PreImage",
     "BinnedImage",
     "ConductanceImage",
+    "SplineImage",
     "confun"
 ]
 
--- icbuilder/splineimage.py
+++ icbuilder/splineimage.py
@@ -1,32 +1,25 @@
 #%% Import
 
 from typing import Optional
-
 import numpy as np
-from secsy import CSgrid, CSprojection
-from datetime import datetime, timedelta
-from scipy.interpolate import griddata
-from copy import deepcopy as dcopy
-from tqdm import tqdm
-from scipy.io import netcdf_file
 from scipy.interpolate import BSpline
-import scipy
 from scipy.sparse import kron, vstack, csc_matrix
 import scipy.sparse as sp
-import scipy.sparse.linalg as spla
-import scipy.optimize as opt
-from scipy.optimize import lsq_linear
-
 from icreader import ConductanceImage
+from sksparse.cholmod import cholesky
+from scipy.io import netcdf_file
+from datetime import datetime
 
 #%%
-class splineImage():
+class SplineImage():
     def __init__(self, 
                  cI: ConductanceImage,
-                 ncp: Optional[int] = 30,
+                 ncp: Optional[int] = 20,
                  ncpt: Optional[int] = None,
                  k: Optional[int] = 3,
-                 kt: Optional[int] = 2):
+                 kt: Optional[int] = 2,
+                 lH: Optional[int] = 0,
+                 lP: Optional[int] = 0):
         
         self.cI = cI                 
         
@@ -34,14 +27,18 @@ class splineImage():
         self.k = k
         self._ncpt = ncpt
         self.kt = kt
+        self.lH = lH
+        self.lP = lP
 
         self.reset_time()
         self.reset_spline()
+        self.reset_model()
+        self.reset_ev()
 
     @property
     def ncpt(self):
         if self._ncpt is None:
-            self._ncpt = self.nt//2
+            self._ncpt = int((self.time[-1] - self.time[0]).total_seconds() / 60 // 5)
         return self._ncpt
 
 #%% Time
@@ -56,7 +53,7 @@ class splineImage():
     @property
     def t(self):
         if self._t is None:
-            self._t = np.array([(t - self.time[0]).total_seconds for t in self.time])
+            self._t = np.array([(t - self.time[0]).total_seconds() for t in self.time])
         return self._t
     
     @property
@@ -66,16 +63,20 @@ class splineImage():
 #%% Grid
 
     @property
-    def x(self):
-        return self.cI.grid.xi
+    def grid(self):
+        return self.cI.grid
     
     @property
-    def eta(self):
-        return self.cI.grid.eta
+    def x(self):
+        return self.grid.xi
+    
+    @property
+    def y(self):
+        return self.grid.eta
     
     @property
     def shape(self):
-        return self.cI.grid.shape
+        return self.grid.shape
     
     @property
     def n(self):
@@ -119,7 +120,7 @@ class splineImage():
 
     @property
     def tknots(self):
-        self.tknots = np.r_[[self.t.min()]*self.kt, np.linspace(self.t.min(), self.t.max(), self.nkt-2*self.kt), [self.t.max()]*self.kt]
+        return np.r_[[self.t.min()]*self.kt, np.linspace(self.t.min(), self.t.max(), self.nkt-2*self.kt), [self.t.max()]*self.kt]
 
     @property
     def G(self):
@@ -133,7 +134,7 @@ class splineImage():
             self._Gt = self.generate_G_3d()
         return self._Gt
         
-    def generate_design_matrix_2d(self, return_sparse=False):
+    def generate_G_2d(self, return_sparse=False):
         G = np.zeros((self.x.size, self.ncp**2))
         for i, (xi, yi) in enumerate(zip(self.x.flatten(), self.y.flatten())):
             Gx = BSpline.design_matrix(xi, self.knots, self.k).todense()
@@ -144,7 +145,7 @@ class splineImage():
             return csc_matrix(G)
         return G
         
-    def generate_design_matrix_3d(self):
+    def generate_G_3d(self):
         Gt = []
         for i, ti in enumerate(self.t):
             Gt_ = BSpline.design_matrix(ti, self.tknots, self.kt)  # Already sparse
@@ -157,191 +158,188 @@ class splineImage():
     def reset_model(self):
         self._mH = None
         self._mP = None
+        self._CpH = None
+        self._CpP = None
+        self._factorH = None
+        self._factorP = None
         
+    @property
+    def factorH(self):
+        if self._factorH is None:
+            self._mH, self._CpH, self._factorH = self.make_model(comp='hall')
+        return self._factorH
+        
+    @property
+    def mH(self):
+        if self._mH is None:
+            self._mH, self._CpH, self._factorH = self.make_model(comp='hall')
+        return self._mH
+    
+    @property
+    def CpH(self):
+        if self._CpH is None:
+            self._mH, self._CpH, self._factorH = self.make_model(comp='hall')
+        return self._CpH
 
+    @property
+    def factorP(self):
+        if self._factorP is None:
+            self._mP, self._CpP, self._factorP = self.make_model(comp='pedersen')
+        return self._factorP    
+    
+    @property
+    def mP(self):
+        if self._mP is None:
+            self._mP, self._CpP, self._factorP = self.make_model(comp='pedersen')
+        return self._mP
+    
+    @property
+    def CpP(self):
+        if self._CpP is None:
+            self._mP, self._CpP, self._factorP = self.make_model(comp='pedersen')
+        return self._CpP    
+    
+    def make_model(self, comp='hall'):
+        if comp == 'hall':
+            d, q, l = self.H.flatten(), self.dH.flatten(), self.lH
+        else:
+            d, q, l = self.P.flatten(), self.dP.flatten(), self.lP
+        
+        f = ~(np.isnan(d) | np.isinf(d) | np.isnan(q) | np.isinf(q))
+    
+        return _make_model(self.Gt[f], d[f], q[f], l)
+    
+#%% Evaluation
+
+    def reset_ev(self):
+        self._pH = None
+        self._dpH = None
+        self._pP = None
+        self._dpP = None
+   
+    @property
+    def pH(self):
+        if self._pH is None:
+            self._pH = self.ev(comp='hall')
+        return self._pH
+    
+    @property
+    def pP(self):
+        if self._pP is None:
+            self._pP = self.ev(comp='pedersen')
+        return self._pP
+    
+    @property
+    def dpH(self):
+        if self._dpH is None:
+            self._dpH = self.ev_uncertainty(comp='hall')
+        return self._dpH
+            
+    @property
+    def dpP(self):
+        if self._dpP is None:
+            self._dpP = self.ev_uncertainty(comp='pedersen')
+        return self._dpP
+
+    def ev(self, comp='hall'):
+        if comp == 'hall':
+            m = self.mH
+        else:
+            m = self.mP
+        return self.Gt.dot(m).reshape((self.nt, self.n, self.n))
+    
+    def ev_uncertainty(self, comp='hall'):
+        if comp == 'hall':
+            Cp = self.CpH
+        else:
+            Cp = self.CpP
+        
+        Gp = self.Gt @ Cp
+        diag_elements = np.array(self.Gt.multiply(Gp).sum(axis=1)).ravel()
+        return np.sqrt(diag_elements).reshape((self.nt, self.n, self.n))
+
+#%% Save image
+    
+    def to_nc(self, filename: str):
+            """
+            Save spline image to a NetCDF file.
+            Can be read/rebuilt using the icReader library.
+    
+            Parameters
+            ----------
+            filename : str
+                Full path to output NetCDF file.
+            """
+            with netcdf_file(filename, 'w') as nc:
+                t, y, x = self.nt, self.n, self.n
+                nc.createDimension('time', t)
+                nc.createDimension('dim1', y)
+                nc.createDimension('dim2', x)
+    
+                nc.createVariable('H',  'f8', ('time', 'dim1', 'dim2'))[:] = self.pH
+                nc.createVariable('P',  'f8', ('time', 'dim1', 'dim2'))[:] = self.pP
+                nc.createVariable('dH', 'f8', ('time', 'dim1', 'dim2'))[:] = self.dpH
+                nc.createVariable('dP', 'f8', ('time', 'dim1', 'dim2'))[:] = self.dpH
+    
+                if self.time is not None:
+                    ref_time = datetime(2000, 1, 1)
+                    time_seconds = np.array([(t - ref_time).total_seconds() for t in self.time], dtype=np.int32)
+                    nc.createVariable("time", np.int32, ("time",))[:] = time_seconds
+                    nc.reference_time = ref_time.strftime("%Y-%m-%dT%H:%M:%S")
+    
+                if self.grid and hasattr(self.grid, "projection"):
+                    nc.position     = self.grid.projection.position.astype(float)
+                    nc.orientation  = self.grid.projection.orientation
+                    nc.L    = self.grid.L
+                    nc.W    = self.grid.W
+                    nc.Lres = self.grid.Lres
+                    nc.Wres = self.grid.Wres
+                    nc.gridR    = self.grid.R
+
+                nc.createDimension('m', self.mH.size)
+                
+                nc.createVariable('mH', 'f8', ('m',))[:] = self.mH
+                nc.createVariable('mP', 'f8', ('m',))[:] = self.mP
+                                
+                L = self.factorH.L()
+                nc.createDimension('LH_nnz', L.nnz)
+                nc.createDimension('ncols_plus_1', self.mH.size+1)
+                nc.createVariable("LH_data", "f4", ("LH_nnz",))[:] = L.data
+                nc.createVariable("LH_indices", "i4", ("LH_nnz",))[:] = L.indices
+                nc.createVariable("LH_indptr", "i4", ("ncols_plus_1",))[:] = L.indptr
+                nc.LH_shape = L.shape
+                nc.createVariable('PH', "i4", ('m',))[:] = self.factorH.P()
+                
+                L = self.factorP.L()
+                nc.createDimension('LP_nnz', L.nnz)
+                nc.createVariable("LP_data", "f4", ("LP_nnz",))[:] = L.data
+                nc.createVariable("LP_indices", "i4", ("LP_nnz",))[:] = L.indices
+                nc.createVariable("LP_indptr", "i4", ("ncols_plus_1",))[:] = L.indptr
+                nc.LP_shape = L.shape
+                nc.createVariable('PP', "i4", ('m',))[:] = self.factorP.P()
+                
+                nc.kt = self.kt
+                nc.nkt = self.nkt
+                
+                nc.createDimension('g1', x*y)
+                nc.createDimension('g2', self.G.shape[1])
+                nc.createVariable('G', float, ('g1', 'g2'))[:] = self.G.todense()
+    
 #%%
-
-        self.H = np.copy(H)
-        self.P = np.copy(P)
-        self.shape = self.H.shape        
-        
-        if Hu is None:
-            #self.Hu = np.stack([np.eye(self.shape[1])*self.shape[0]])
-            self.Hu = np.ones(self.shape)
-        else:
-            self.Hu = np.copy(Hu)
-        
-        if Pu is None:
-            #self.Pu = np.stack([np.eye(self.shape[1])*self.shape[0]])
-            self.Pu = np.ones(self.shape)
-        else:
-            self.Pu = np.copy(Pu)
-        
-        self.grid = dcopy(grid)
-        
-        # Define dimensions
-        self.n = self.shape[1]
-        self.nt = self.shape[0] # time dimension
-        
-        # Grid coordinates
-        self.x = np.copy(self.grid.xi)
-        self.y = np.copy(self.grid.eta)
-        
-        if t is None:
-            self.t = np.arange(self.nt)
-        else:
-            self.t = t
-        
-        self.ncp = ncp
-        self.ncpt = ncpt
-        self.k = k
-        self.kt = kt
-        
-        # Amount of knots
-        self.nk = ncp + k + 1
-        self.nkt = ncpt + kt + 1 # time dimension
-        
-        # Define truncated knot location
-        self.knots = np.r_[[self.x.min()]*self.k, np.linspace(self.x.min(), self.x.max(), self.nk-2*self.k), [self.x.max()]*self.k]
-        self.tknots = np.r_[[self.t.min()]*self.kt, np.linspace(self.t.min(), self.t.max(), self.nkt-2*self.kt), [self.t.max()]*self.kt]
     
-    def generate_design_matrix_2d(self):
-        self.Gs = np.zeros((self.x.size, self.ncp**2))
-        for i, (xi, yi) in enumerate(zip(self.x.flatten(), self.y.flatten())):
-            Gx = BSpline.design_matrix(xi, self.knots, self.k).todense()
-            Gy = BSpline.design_matrix(yi, self.knots, self.k).todense()
-            Gy = np.kron(np.eye(self.ncp), Gy)
-            self.Gs[i, :] = Gx.dot(Gy)
+def _make_model(G, d, q, l):
+    GTQinv = G.T.multiply(1/q)  # Sparse multiplication
+
+    GTG = GTQinv @ G  # Stays sparse
+    reg_term = 10**l * np.median(GTG.diagonal()) * sp.eye(GTG.shape[0], format='csr')
+    GTG = GTG + reg_term  # Sparse regularization
+
+    GTd = GTQinv @ d  # Stays dense
+
+    factor = cholesky(GTG.tocsc())       # Performs sparse Cholesky
+    m = factor(GTd)              # Solve for m
+    Cp = factor.inv()            # Full posterior covariance
+
+    return m, Cp, factor
         
-    def generate_design_matrix_3d(self):        
-        self.Gs_s = csc_matrix(self.Gs)
-        G = []
-        for i, ti in enumerate(self.t):
-            Gt = BSpline.design_matrix(ti, self.tknots, self.kt)  # Already sparse
-            Gt = kron(np.eye(self.Gs.shape[1]), Gt, format='csr')  # Ensure sparse output
-            G.append(self.Gs_s @ Gt)  # Matrix multiplication remains sparse
-        self.G = vstack(G, format='csr')  # Efficient sparse stacking
-
-    
-    def make_models(self, lH=0, lP=0):
-        self.lH = lH
-        self.lP = lP
-        
-        if not hasattr(self, 'G'):
-            self.generate_design_matrix_3d()
-
-        def make_model_sparse(G, d, C, l=0, nnls=False):
-            Cinv = 1 / C  # Element-wise inverse
-            GTCinv = G.T.multiply(Cinv)  # Sparse multiplication
-    
-            GTG = GTCinv @ G  # Stays sparse
-            reg_term = l * np.median(GTG.diagonal()) * sp.eye(GTG.shape[0], format='csr')
-            GTG = GTG + reg_term  # Sparse regularization
-
-            GTd = GTCinv @ d  # Stays dense
-            
-            # Solve GTG * m = GTd directly (no explicit inverse)
-            print('Start solve')
-            if nnls:
-                m = lsq_linear(GTG, GTd, bounds=(0, np.inf), method='trf').x
-            else:
-                m = spla.spsolve(GTG, GTd)
-            print('End solve')
-            
-            print('Start post')
-            # Compute posterior covariance (C_m)
-            #solver = spla.factorized(GTG)  # Factorize once
-            #Cp = np.column_stack([solver(np.eye(GTG.shape[0])[:, i]) for i in range(GTG.shape[0])])
-            print('End post')
-
-            #return m, Cp
-            return m, 0
-
-        def make_model(G, d, C, l=0):
-            Cinv = 1 / C  # Element-wise inverse
-            GTCinv = G.T.multiply(Cinv)  # Sparse multiplication
-    
-            GTG = (GTCinv @ G).todense()  # Stays sparse
-            #reg_term = l * np.median(GTG.diagonal()) * sp.eye(GTG.shape[0], format='csr')
-            reg_term = l * np.median(np.diag(GTG)) * np.eye(GTG.shape[0])
-            GTG += reg_term  # Sparse regularization
-
-            GTd = GTCinv @ d  # Stays dense
-
-            # Solve for Cp efficiently (Cp = GTG^{-1})
-            Cp = scipy.linalg.lstsq(GTG, np.eye(GTG.shape[0]), lapack_driver='gelsy')[0]  # Uses sparse solver
-
-            # Solve for m (m = Cp @ GTd)
-            m = Cp.dot(GTd)
-
-            return m, Cp
-
-        def make_model_nnls(G, d, C, l=0):
-            Cinv = 1 / C
-            GTCinv = G.T.multiply(Cinv)  # Sparse multiplication
-            
-            GTG = (GTCinv @ G).todense()  # Stays sparse
-            reg_term = l * np.median(np.diag(GTG)) * np.eye(GTG.shape[0])
-            GTG += reg_term  # Sparse regularization
-            
-            GTd = GTCinv @ d  # Stays dense
-            
-            # Solve NNLS
-            m, _ = opt.nnls(GTG, GTd)
-
-            # Identify active parameters
-            active_idx = np.where(m > 1e-10)[0]  # Only consider nonzero parameters
-
-            # Compute posterior covariance only for active parameters
-            if len(active_idx) > 0:
-                GTG_active = GTG[np.ix_(active_idx, active_idx)]
-                Cp_active = scipy.linalg.inv(GTG_active)  # Inverse of the active part
-            else:
-                Cp_active = np.zeros((GTG.shape[0], GTG.shape[0]))
-
-            #return m, Cp_active, active_idx
-            return m, Cp_active
-
-        f = ~(np.isnan(self.H) | np.isinf(self.H) | np.isnan(self.Hu) | np.isinf(self.Hu)).flatten()
-        #self.mH, self.CpH = make_model(self.G[f, :], self.H.flatten()[f], self.Hu.flatten()[f], self.lH)
-        #self.mH, self.CpH = make_model_sparse(self.G[f, :], self.H.flatten()[f], self.Hu.flatten()[f], self.lH) # Remove nans
-        H = self.H.flatten()
-        Hu = self.Hu.flatten()
-        H[~f] = 0
-        Hu[~f] = 1
-        self.mH, self.CpH = make_model_sparse(self.G, H, Hu, self.lH)
-        #self.mP, self.CpP = self.mH, self.CpH
-        
-        P = self.P.flatten()
-        Pu = self.Pu.flatten()
-        P[~f] = 0
-        Pu[~f] = 1
-        self.mP, self.CpP = make_model_sparse(self.G, P, Pu, self.lP)
-        
-        #f = ~(np.isnan(self.P) | np.isinf(self.P) | np.isnan(self.Pu) | np.isinf(self.Pu)).flatten()
-        #self.mP, self.CpP = make_model(self.G[f, :], self.P.flatten()[f], self.Pu.flatten()[f], self.lP)
-    
-    def eval(self, m, Cp=None):
-        pred = self.G.dot(m).reshape(self.shape)
-        if Cp is None:
-            return pred
-        
-        sig = np.zeros(self.G.shape[0])
-        for i in range(sig.size):
-            sig[i] = self.G[i, :].dot(Cp).dot(self.G[i, :].T)
-        
-        return pred, sig
-    
-    def eval_Hall(self, uncertainty=False):
-        if uncertainty:            
-            pred, sig = self.eval(self.mH, self.CpH)
-            return pred, sig
-        pred = self.eval(self.mH)
-        return pred
-    
-    def eval_Pedersen(self, uncertainty=False):
-        if uncertainty:            
-            pred, sig = self.eval(self.mP, self.CpP)
-            return pred, sig
-        pred = self.eval(self.mP)
-        return pred
+     
