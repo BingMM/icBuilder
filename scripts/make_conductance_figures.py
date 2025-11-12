686718a9ca02ac701edaf4fec7c7463b79a85dc5
Karl Laundal <kalau@space.dtu.dk>
2025-11-12 17:44:24 +0100

    changed color scales

--- scripts/make_conductance_figures.py
+++ scripts/make_conductance_figures.py
@@ -11,8 +11,8 @@ from icreader import ConductanceImage
 
 #%% Paths
 
-base = '/Home/siv32/mih008/repos/icBuilder/example_data/'
-base = '/disk/IMAGE_FUV/fuv/'
+base = '/Users/laundal/Library/CloudStorage/Dropbox/git/tmp/icBuilder/example_data/'
+#base = '/Users/laundal/tmp/'
 
 p_in = base + 'conductance/'
 p_out = base + 'figures/conductance/'
@@ -31,16 +31,16 @@ def get_c_scales(cI):
                 's12s': (0, np.round(np.nanmax(cI.s12_std)+1)),
                 's13m': (0, np.round(np.nanmax(cI.s13_avg)+1)),
                 's13s': (0, np.round(np.nanmax(cI.s13_std)+1)),
-                'E0':   (0,  25),
+                'E0':   (0,  10),
                 'dE0':  (0, np.round(np.nanmax(cI.dE0)+1)),
                 'Fe':   (0, np.round(np.nanmax(cI.Fe)+1)),
                 'dFe':  (0, np.round(np.nanmax(cI.dFe)+1)),
-                'R':    (0, 150),
+                'R':    (0, 300),
                 'dR':   (0, np.round(5*np.median(cI.dR[~np.isnan(cI.dR)])+1)),
-                'H':    (0, np.round(np.nanmax(cI.H)+1)),
-                'dH':   (0, np.round(np.nanmax(cI.dH)+1)),
-                'P':    (0, np.round(np.nanmax(cI.P)+1)),
-                'dP':   (0, np.round(np.nanmax(cI.dP)+1))
+                'H':    (0, np.round(np.nanmax(cI.P)+1)),
+                'dH':   (0, np.round(np.nanmax(cI.dH/5)+1)),
+                'P':    (0, np.round(np.nanmax(cI.P/2)+1)),
+                'dP':   (0, np.round(np.nanmax(cI.dP/5)+1))
                 }
     return c_scales
     
@@ -79,10 +79,11 @@ def plot(cI, i, c_scales, lat, lt):
 
 plt.ioff()
 for orbit in tqdm(o, total=len(o)):
-    if orbit < 560: # Continue after crash
-        continue
+    #if orbit < 560: # Continue after crash
+    #    continue
     filename = p_in + f'or_{str(orbit).zfill(4)}.nc'
-    
+    print(filename)
+
     cI = ConductanceImage(filename)
     
     c_scales = get_c_scales(cI)
